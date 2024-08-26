7ddd2face5b91b12c8fcc4253869de1218139eba
anjeetpatel <anjeetpatel0306@gmail.com>
2024-08-26 16:02:06 +0530

    operator

---
+++ operator.php
@@ -0,0 +1,16 @@
+<?php
+$a = 5;
+$b = 10;
+$c = 2;
+
+$result = $a+$b*$c >=20? $a++ : --$b+$c;
+
+$x=4;
+$y=2;
+$z=3;
+$result2 = 
+$result2 = $x + $y *$z ** $y/$x-$y%$z;
+echo "Result1 = $result";
+echo"<br>";
+echo "Result2 = $result2";
+?>
