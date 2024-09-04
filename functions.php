efb1e7119b9b52f82e92cd3749596d7122f17d61
anjeetpatel <anjeetpatel0306@gmail.com>
2024-09-04 21:42:11 +0530

    function

---
+++ functions.php
@@ -0,0 +1,14 @@
+<!-- create a function that returns multiple values using an array -->
+ <?php
+function getMultipleValues() {
+    // define the values to be returned
+    $value1 = "Hello";
+    $value2 = "World";
+    
+    // create an array to store the values
+    $values = array($value1, $value2);
+    
+    // return the array
+    return $values;
+}
+?>
