f33d38fd2a83521a5ad1b9edc1d136c22f5fe5c4
anjeetpatel <anjeetpatel0306@gmail.com>
2024-10-09 15:50:28 +0530

    predefined

---
+++ task.php
@@ -0,0 +1,13 @@
+<?php
+// String and pattern for the search
+$string = "Hello, welcome to the PHP world!";
+$pattern = "/php/i";  // Case-insensitive search for 'php'
+
+// Perform case-insensitive search with position capture
+if (preg_match($pattern, $string, $matches, PREG_OFFSET_CAPTURE)) {
+    echo "Match found: " . $matches[0][0] . "<br>";
+    echo "Position of match: " . $matches[0][1];
+} else {
+    echo "No match found.";
+}
+?>
