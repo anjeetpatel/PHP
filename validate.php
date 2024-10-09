953b0be0ec68462729120a65bc6d0c50d9f4dc48
anjeetpatel <anjeetpatel0306@gmail.com>
2024-10-09 16:21:54 +0530

    validatename

--- validate.php
+++ validate.php
@@ -1,5 +1,12 @@
 <!-- take a input string from the user and validate it  -->
 <?php
+// Sample input string
+$input = "Hello, welcome to the PHP world!";
 
-
+// Validate the input string
+if (preg_match("/php/i", $input)) {
+    echo "The input string contains 'php'.";
+} else {
+    echo "The input string does not contain 'php'.";
+}
 ?>
